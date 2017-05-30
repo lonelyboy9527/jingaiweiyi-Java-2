c7e436d861fb60bd0c4df22d04b8585cb6877b16
tangfei <lonelyboy9527@sina.com>
2017-05-30 22:42:07 +0800

    复习

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,13 +5,15 @@
  * tab 					:缩进
  * shift + tab 			:退格
  * 
+ * control + command + /选择注释
  * 
- * 
+ * command + / 			注释
  * */
 
 
 public class HelloWorld {
 	// syso按回车即可
+	
 	public static void printfMessageHelloWorld() {
 		System.out.println("HelloWorld Class Hello World!");
 	}
